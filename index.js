5c7d5325c3d257ad44e22b68d3ed96b80782ac5a
unknown <anwarhosen575@gmail.com>
2021-01-23 16:13:09 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+
+// 1.	kilometerToMeter
+
+
+function kilometerToMeter(inch){
+    var Meter = kilometer* 1000;
+    return feet;
+}
+var result = kilometerToMeter(10);
+console.log(result);
+
+// 2.	budgetCalculator
+
+var mobile = 10000;
+var clock = 5400;
+var laptop = 78000;
+
+budgetCalculator = mobile + clock + laptop;
+console.log(budgetCalculator);
